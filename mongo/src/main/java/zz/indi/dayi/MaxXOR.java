36a93047d2d72f0fade4f656b3c72a9f6dea97b8
zz <zhangzhen@trgroup.cn>
2024-05-28 17:06:08 +0800

    update

--- mongo/src/main/java/zz/indi/dayi/MaxXOR.java
+++ mongo/src/main/java/zz/indi/dayi/MaxXOR.java
@@ -7,101 +7,89 @@ public class MaxXOR {
     public static void main(String[] args) {
 
         System.out.println(new MaxXOR().findMaximumXOR(new int[]{0}));
+        System.out.println(new MaxXOR().findMaximumXOR(new int[]{3}));
         System.out.println(new MaxXOR().findMaximumXOR(new int[]{2, 4}));
         System.out.println(new MaxXOR().findMaximumXOR(new int[]{16, 8}));
-        System.out.println(new MaxXOR().findMaximumXOR(new int[]{8, 10 , 2}));
+        System.out.println(new MaxXOR().findMaximumXOR(new int[]{8, 10, 2}));
     }
 
     public int findMaximumXOR(int[] nums) {
         int r = 0;
 
         Trie trie = new Trie();
-        trie.add(nums[0]);
 
-        for (int i = 1; i < nums.length; i++) {
-            int[] bit = bit(nums[i]);
-
-            int tmp = 0;
-            Trie.TrieNode tn = trie.root;
-            for (int k = 0; k < bit.length; k++) {
-                Trie.TrieNode tt;
-
-                if (tn.child[1] == null) {
-                    tt = tn.child[0];
-                } else if (tn.child[0] == null) {
-                    tt = tn.child[1];
-                } else {
-                    tt = tn.child[1 ^ bit[k]];
-//                    if (bit[k] == 0) {
-//                        tt = tn.child[1];
-//                    } else {
-//                        tt = tn.child[0];
-//                    }
-                }
-
-                tmp = (tmp << 1 | (tt.v ^ bit[k]));
-                tn = tt;
-            }
-            r = Math.max(r, tmp);
-
+        for (int i = 0; i < nums.length; i++) {
             trie.add(nums[i]);
+            r = Math.max(r, trie.get(nums[i]));
+
+//            r = Math.max(r, trie.get(nums[i]));
+//            trie.add(nums[i]);
         }
 
         return r;
     }
 
-    private static int[] bit(int num) {
-        int[] bit = new int[32];
-        Arrays.fill(bit, 0);
-
-        int i = 31;
-        while (num > 0) {
-            bit[i--] = (num & 1) == 1 ? 1 : 0;
-            num = num >> 1;
-        }
-
-        return bit;
-    }
-
     static class Trie {
 
-        void print(TrieNode cur) {
-            System.out.printf("%d", cur.v);
+        void print() {
+            print(this.root, new StringBuilder());
+        }
+        void print(TrieNode cur, StringBuilder pre) {
             if (cur.child[0] == null && cur.child[1] == null) {
-                System.out.println();
+                System.out.printf("%s%d\n", pre, cur.v);
             }
+            pre.append(cur.v);
             if (cur.child[0] != null) {
-                print(cur.child[0]);
+                print(cur.child[0], pre);
             }
             if (cur.child[1] != null) {
-                print(cur.child[1]);
+                print(cur.child[1], pre);
             }
+            pre.deleteCharAt(pre.length() - 1);
         }
 
         TrieNode root;
-
 
         public Trie() {
             this.root = new TrieNode();
         }
 
         public void add(int num) {
-            int[] bit = bit(num);
-
-//            for (int i = 0; i < bit.length; i++) {
-//                System.out.printf("%d", bit[i]);
-//            }
-//            System.out.println(" >>>>>>>>> " + num);
-
             TrieNode p = this.root;
-            for (int i = 0; i < bit.length; i++) {
-                if (p.child[bit[i]] == null) {
-                    p.child[bit[i]] = new TrieNode();
-                    p.child[bit[i]].v = bit[i];
+
+            for (int i = 31; i >= 0; i--) {
+                int v = num >> i & 1;
+                if (p.child[v] == null) {
+                    p.child[v] = new TrieNode();
+                }
+                p.child[v].v = v;
+                p = p.child[v];
+            }
+        }
+
+        public int get(int num) {
+            TrieNode cur = this.root;
+            if (cur.child[0] == null && cur.child[1] == null) {
+                return 0;
+            }
+
+            int res = 0;
+            for (int i = 31; i >= 0; i--) {
+                int v = num >> i & 1;
+
+                TrieNode t;
+                if (cur.child[0] == null) {
+                    t = cur.child[1];
+                } else if (cur.child[1] == null) {
+                    t = cur.child[0];
+                } else {
+                    t = cur.child[1 - v];
                 }
 
-                p = p.child[bit[i]];
+                res |= (t.v ^ v) << i;
+                cur = t;
             }
+            return res;
         }
 
         class TrieNode {
